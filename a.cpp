0daf16f880b1d44db2aebf6357cbaaf15654ec1e
Raghav Chugh <92471414+RC1105@users.noreply.github.com>
2022-11-07 19:18:04 +0530

    cpp file uploaded

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+//Q. To take two elements from an array and return numbers with maximum difference
+int main(){
+    vector<int> arr{1,4,5,68,2,1,5,88,9,100};
+    int minn=INT_MAX, maxx=INT_MIN;
+    for(int i=0; i<arr.size();i++){
+        if(minn>arr[i]){
+            minn=arr[i];
+        }
+        if(maxx<arr[i]){
+            maxx=arr[i];
+        }
+    }
+    cout<<"Max diff is: "<< (maxx-minn)<<endl;
+    cout<<"The numbers are "<<maxx<<" and "<<minn;
+}
